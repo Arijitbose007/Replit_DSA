408b2b0219b1c01e85fc634c356334a78653ac9a
ARIJIT BOSE <61642341+Arijitbose007@users.noreply.github.com>
2024-03-10 18:28:59 +0000

    small change

--- .ccls-cache/@home@runner@DSA/nD_Array@main.cpp
+++ .ccls-cache/@home@runner@DSA/nD_Array@main.cpp
@@ -1,11 +1,11 @@
-#include <bits/stdc++.h> 
+#include <bits/stdc++.h>
 using namespace std;
 
-void arrayOfVectors(int,int);
-void vectorOfVectors(int,int);
-int main(){
-  int m = 2 , n = 3;
-  arrayOfVectors(m,n);
-  vectorOfVectors(m,n);
+void arrayOfVectors(int, int);
+void vectorOfVectors(int, int);
+int main() {
+  int m = 2, n = 3;
+  arrayOfVectors(m, n);
+  vectorOfVectors(m, n);
   return 0;
 }
--- .ccls-cache/@home@runner@DSA/nD_Array@vectorOfVectors.cpp
+++ .ccls-cache/@home@runner@DSA/nD_Array@vectorOfVectors.cpp
@@ -4,11 +4,11 @@ using namespace std;
 void vectorOfVectors(int m, int n) {
   vector<vector<int>> arr;
   for (int i = 0; i < m; i++) {
-    vector<int> v;  // Move the declaration here
+    vector<int> v; // Move the declaration here
     for (int j = 0; j < n; j++) {
       v.push_back(i + 1);
     }
-    arr.push_back(v);  // Push the vector v once after it's populated.
+    arr.push_back(v); // Push the vector v once after it's populated.
   }
   for (int i = 0; i < arr.size(); i++) {
     for (int j = 0; j < arr[i].size(); j++) {
